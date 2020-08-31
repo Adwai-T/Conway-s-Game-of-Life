bed1283d0da40c6ffa434a0415d732701c55b7d1
Adwai-T <waitforadi@gmail.com>
2020-08-31 18:46:39 +0530

    Better Format

--- Main.cpp
+++ Main.cpp
@@ -5,148 +5,168 @@
 
 #include "PreFabs.cpp"
 
-constexpr unsigned int WINDOW_X{ 800 }, WINDOW_Y{ 600 }, TILE_SIZE{ 10 }, GENERATIONS_PER_SEC{ 15 };
-const sf::Vector2f TILE_RECT_SIZE{ 10, 10 };
-const std::string title { "Game Of life" };
+constexpr unsigned int WINDOW_X{800}, WINDOW_Y{600}, TILE_SIZE{10}, GENERATIONS_PER_SEC{15};
+const sf::Vector2f TILE_RECT_SIZE{10, 10};
+const std::string title{"Game Of life"};
 
 class GameOfLife
 {
-  private:
-    std::vector<sf::RectangleShape> rectangles;
-    std::array<std::array<bool, WINDOW_X/TILE_SIZE+2>,WINDOW_Y/TILE_SIZE+2> aliveCells; 
-    
-  public:
-    GameOfLife()
+private:
+  std::vector<sf::RectangleShape> rectangles;
+  std::array<std::array<bool, WINDOW_X / TILE_SIZE + 2>, WINDOW_Y / TILE_SIZE + 2> aliveCells;
+
+public:
+  GameOfLife()
+  {
+    reset_aliveCells();
+  }
+  ~GameOfLife()
+  {
+  }
+
+  void reset_aliveCells()
+  {
+    for (int row = 0; row < WINDOW_Y / TILE_SIZE + 2; row++)
     {
-      reset_aliveCells();
+      for (int col = 0; col < WINDOW_X / TILE_SIZE + 2; col++)
+      {
+        aliveCells[row][col] = false;
+      }
     }
-    ~GameOfLife()
+  }
+
+  std::vector<sf::RectangleShape> getrectangles()
+  {
+    return rectangles;
+  }
+
+  bool addRectangle(int x, int y)
+  {
+
+    if (x >= 0 && y >= 0 && x <= WINDOW_X - TILE_SIZE && y <= WINDOW_Y - TILE_SIZE)
     {
-      
+      x = (x / TILE_SIZE) * TILE_SIZE;
+      y = (y / TILE_SIZE) * TILE_SIZE;
+
+      for (sf::RectangleShape nowRect : rectangles)
+      {
+        if (nowRect.getPosition().x == x && nowRect.getPosition().y == y)
+          return false;
+      }
+
+      sf::RectangleShape rect;
+      rect.setSize(TILE_RECT_SIZE);
+      rect.setPosition(x, y);
+      rect.setFillColor(sf::Color{50, 168, 82, 255});
+      rectangles.push_back(rect);
+
+      return true;
+    }
+  }
+
+  void update_aliveCells()
+  {
+    for (sf::RectangleShape rect : rectangles)
+    {
+      aliveCells[(rect.getPosition().y / TILE_SIZE) + 1][(rect.getPosition().x / TILE_SIZE) + 1] = true;
+    }
+  }
+
+  //For Debugging.
+  // void print_aliveCells()
+  // {
+  //   for(std::array<bool, WINDOW_Y/TILE_SIZE+2> arr : aliveCells)
+  //   {
+  //     for(bool alive : arr)
+  //     {
+  //       std::cout << alive << " ";
+  //     }
+  //     std::cout << std::endl;
+  //   }
+  // }
+
+  unsigned int getNumberOfSurroundingCells(int row, int col)
+  {
+    unsigned int surrounding = 0;
+
+    if (aliveCells[row - 1][col - 1])
+    {
+      surrounding++;
+    }
+    if (aliveCells[row - 1][col])
+    {
+      surrounding++;
+    }
+    if (aliveCells[row - 1][col + 1])
+    {
+      surrounding++;
+    }
+    if (aliveCells[row][col - 1])
+    {
+      surrounding++;
+    }
+    if (aliveCells[row][col + 1])
+    {
+      surrounding++;
+    }
+    if (aliveCells[row + 1][col - 1])
+    {
+      surrounding++;
+    }
+    if (aliveCells[row + 1][col])
+    {
+      surrounding++;
+    }
+    if (aliveCells[row + 1][col + 1])
+    {
+      surrounding++;
     }
 
-    void reset_aliveCells()
+    return surrounding;
+  }
+
+  void nextGeneration()
+  {
+    reset_aliveCells();
+    update_aliveCells();
+    rectangles.clear();
+
+    unsigned int surrounding = 0;
+
+    for (int row = 1; row <= WINDOW_Y / TILE_SIZE; row++)
     {
-      for(int row = 0; row < WINDOW_Y/TILE_SIZE+2; row++)
+      for (int col = 1; col <= WINDOW_X / TILE_SIZE; col++)
       {
-        for(int col = 0; col < WINDOW_X/TILE_SIZE+2; col++)
+        surrounding = getNumberOfSurroundingCells(row, col);
+
+        if (surrounding == 3)
         {
-          aliveCells[row][col] = false;
+          addRectangle((col - 1) * TILE_SIZE, (row - 1) * TILE_SIZE);
+        }
+        else if (surrounding == 2 && aliveCells[row][col])
+        {
+          addRectangle((col - 1) * TILE_SIZE, (row - 1) * TILE_SIZE);
         }
       }
     }
+  }
 
-    std::vector<sf::RectangleShape> getrectangles()
+  void addPrefab(std::vector<Prefabs::Point_Cordinate> prefab_vector)
+  {
+    for (Prefabs::Point_Cordinate point : prefab_vector)
     {
-      return rectangles;
+      addRectangle(point.x, point.y);
     }
+  }
 
-    bool addRectangle(int x, int y)
-    {
-
-      if(x >= 0 && y >= 0  && x <= WINDOW_X-TILE_SIZE && y <= WINDOW_Y-TILE_SIZE)
-      {
-        x = (x/TILE_SIZE) * TILE_SIZE;
-        y = (y/TILE_SIZE) * TILE_SIZE;
-
-        for(sf::RectangleShape nowRect : rectangles)
-        {
-          if(nowRect.getPosition().x == x && nowRect.getPosition().y == y)
-            return false;
-        }
-
-        sf::RectangleShape rect;
-        rect.setSize(TILE_RECT_SIZE);
-        rect.setPosition(x, y);
-        rect.setFillColor(sf::Color{ 50, 168, 82, 255 });
-        rectangles.push_back(rect);
-
-        return true;
-      }
-    }
-
-    void update_aliveCells()
-    {
-      for(sf::RectangleShape rect : rectangles)
-      {
-        aliveCells[(rect.getPosition().y/TILE_SIZE) + 1][(rect.getPosition().x/TILE_SIZE) + 1] = true;
-      }
-    }
-
-//For Debugging.
-    // void print_aliveCells()
-    // {
-    //   for(std::array<bool, WINDOW_Y/TILE_SIZE+2> arr : aliveCells)
-    //   {
-    //     for(bool alive : arr)
-    //     {
-    //       std::cout << alive << " ";
-    //     }
-    //     std::cout << std::endl;
-    //   }
-    // }
-
-    unsigned int getNumberOfSurroundingCells(int row, int col)
-    {
-      unsigned int surrounding = 0;
-
-      if(aliveCells[row-1][col-1]){ surrounding++; }
-      if(aliveCells[row-1][col]){ surrounding++; }
-      if(aliveCells[row-1][col+1]){ surrounding++; }
-      if(aliveCells[row][col-1]){ surrounding++; }
-      if(aliveCells[row][col+1]){ surrounding++; }
-      if(aliveCells[row+1][col-1]){ surrounding++; }
-      if(aliveCells[row+1][col]){ surrounding++; }
-      if(aliveCells[row+1][col+1]){ surrounding++; }
-      
-      return surrounding;
-    }
-      
-
-    void nextGeneration()
-    {
-      reset_aliveCells();
-      update_aliveCells();
-      rectangles.clear();
-
-      unsigned int surrounding = 0;
-
-      for(int row = 1; row <= WINDOW_Y/TILE_SIZE; row++)
-      {
-        for(int col = 1; col <= WINDOW_X/TILE_SIZE; col++)
-        {
-          surrounding = getNumberOfSurroundingCells(row, col);
-
-          if(surrounding == 3)
-          {
-            addRectangle((col-1)*TILE_SIZE, (row-1)*TILE_SIZE);
-
-          }else if(surrounding == 2 && aliveCells[row][col])
-          {
-            addRectangle((col-1)*TILE_SIZE, (row-1)*TILE_SIZE);
-          }
-        }
-      }
-    }
-
-    void addPrefab(std::vector<Prefabs::Point_Cordinate> prefab_vector)
-    {
-      for(Prefabs::Point_Cordinate point : prefab_vector)
-      {
-        addRectangle(point.x, point.y);
-      }
-    }
-
-    void clearRectangle()
-    {
-      rectangles.clear();
-    }
-
+  void clearRectangle()
+  {
+    rectangles.clear();
+  }
 };
 
 int main()
 {
-
   sf::RenderWindow window(sf::VideoMode(WINDOW_X, WINDOW_Y), title, sf::Style::Close);
 
   //Game
@@ -156,8 +176,8 @@ int main()
   //Text
   sf::Font font;
   sf::Text game_state;
-  const std::string pause{ "Paused" };
-  const std::string playing{ "Playing" };
+  const std::string pause{"Paused"};
+  const std::string playing{"Playing"};
 
   //Font
   try
@@ -168,21 +188,20 @@ int main()
     game_state.setFont(font);
     game_state.setCharacterSize(25);
     game_state.setPosition(5, 5);
-
-  }catch(std::exception e)
+  }
+  catch (std::exception e)
   {
     std::cout << "Font could not be Loaded." << std::endl;
   }
 
-
   //Game Loop
-  while(window.isOpen())
+  while (window.isOpen())
   {
     sf::Event event;
 
-    while(window.pollEvent(event))
+    while (window.pollEvent(event))
     {
-      if(event.type == sf::Event::Closed)
+      if (event.type == sf::Event::Closed)
         window.close();
     }
 
@@ -190,7 +209,7 @@ int main()
     window.clear();
 
     //Left mouse button to add Tiles one at a time.
-    if(sf::Mouse::isButtonPressed(sf::Mouse::Left))
+    if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
     {
       int x = sf::Mouse::getPosition(window).x;
       int y = sf::Mouse::getPosition(window).y;
@@ -198,34 +217,34 @@ int main()
     }
 
     //Right Mouse Button to add Prefab selected.
-    if(sf::Mouse::isButtonPressed(sf::Mouse::Right))
+    if (sf::Mouse::isButtonPressed(sf::Mouse::Right))
     {
       int x = sf::Mouse::getPosition(window).x;
       int y = sf::Mouse::getPosition(window).y;
       game.addPrefab(
-        prefab_factory.getPrefabByName(
-          x, y, TILE_SIZE, prefab_factory.glider, (int)prefab_factory.selected_prefab_number));
+          prefab_factory.getPrefabByName(
+              x, y, TILE_SIZE, prefab_factory.glider, (int)prefab_factory.selected_prefab_number));
     }
 
     //Middle Mouse button to Select the prefab
-    prefab_factory.selectPrefab(window, event,TILE_SIZE);
+    prefab_factory.selectPrefab(window, event, TILE_SIZE);
 
-    for(sf::RectangleShape rect : game.getrectangles())
+    for (sf::RectangleShape rect : game.getrectangles())
     {
       window.draw(rect);
     }
 
-    if(sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
     {
       game_state.setString(pause);
     }
 
-    if(sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
     {
       game_state.setString(playing);
     }
 
-    if(game_state.getString() == playing)
+    if (game_state.getString() == playing)
     {
       game.nextGeneration();
     }
@@ -235,9 +254,8 @@ int main()
     window.display();
 
     //Pause thread to control framerate.
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000/GENERATIONS_PER_SEC));
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000 / GENERATIONS_PER_SEC));
   }
 
   return EXIT_SUCCESS;
-
 }
--- PreFabs.cpp
+++ PreFabs.cpp
@@ -11,173 +11,179 @@ namespace Prefabs
 {
     class Point_Cordinate
     {
-        public:
-            unsigned int x;
-            unsigned int y;
+    public:
+        unsigned int x;
+        unsigned int y;
 
-            Point_Cordinate(unsigned int x, unsigned int y)
-            {
-                this->x = x;
-                this->y = y;
-            }
+        Point_Cordinate(unsigned int x, unsigned int y)
+        {
+            this->x = x;
+            this->y = y;
+        }
     };
 
     class PreFabs_Factory
     {
-        private:
-            std::vector<Point_Cordinate> selectedVector{};
+    private:
+        std::vector<Point_Cordinate> selectedVector{};
 
         //TODO : Add rotation transformation method
-            // const unsigned int* rotate(float angle, const unsigned int prefab_array[], const unsigned int size)
-            // {
-            //     unsigned int returnArray[size];
+        // const unsigned int* rotate(float angle, const unsigned int prefab_array[], const unsigned int size)
+        // {
+        //     unsigned int returnArray[size];
 
-            //     for(int i = 0; i < size; i=i+2)
-            //     {
-            //         returnArray[i] = 
-            //     }
-            // }
+        //     for(int i = 0; i < size; i=i+2)
+        //     {
+        //         returnArray[i] =
+        //     }
+        // }
 
-            //Size of array of prefab
-            const unsigned int prefab_glider_size{ 10 };
-            //Square grid that represents the glider. In this case 3*3
-            const unsigned int prefab_glider_gridsize{ 3 };
-            //x, y cordinates in sequence.
-            const unsigned int prefab_glider[10]
+        //Size of array of prefab
+        const unsigned int prefab_glider_size{10};
+        //Square grid that represents the glider. In this case 3*3
+        const unsigned int prefab_glider_gridsize{3};
+        //x, y cordinates in sequence.
+        const unsigned int prefab_glider[10]{
+            0, 0, 2, 0,
+            1, 1, 2, 1,
+            1, 2};
+
+        const unsigned int prefab_lightweight_spaceship_size{24};
+        const unsigned int prefab_lightweight_spaceship_gridsize{5};
+        const unsigned int prefab_lightweight_spaceship[24]{
+            2, 0, 3, 0,
+            0, 1, 1, 1, 3, 1, 4, 1,
+            0, 2, 1, 2, 2, 2, 3, 2,
+            1, 3, 2, 3};
+
+        const unsigned int prefab_middleweight_spaceship_size{22};
+        const unsigned int prefab_middleweight_spaceship_gridsize{6};
+        const unsigned int prefab_middleweight_spaceship[22]{
+            0, 0, 1, 0, 2, 0, 3, 0, 4, 0,
+            0, 1, 5, 1,
+            0, 2,
+            1, 3, 5, 3,
+            3, 4};
+
+        const unsigned int prefab_heavy_spaceship_size{26};
+        const unsigned int prefab_heavy_spaceship_gridsize{7};
+        const unsigned int prefab_heavy_spaceship[26]{
+            0, 0, 1, 0, 2, 0, 3, 0, 4, 0, 5, 0,
+            0, 1, 6, 1,
+            0, 2,
+            1, 3, 6, 3,
+            3, 4, 4, 4};
+
+        std::vector<Point_Cordinate> get_prefab(unsigned int x,
+                                                unsigned int y,
+                                                unsigned int tile_size,
+                                                const unsigned int prefab[],
+                                                unsigned int size)
+        {
+            std::vector<Point_Cordinate> returnVector;
+
+            x = (x / tile_size) * tile_size;
+            y = (y / tile_size) * tile_size;
+
+            for (int i = 0; i < size; i = i + 2)
             {
-                0,0, 2,0,
-                1,1, 2,1,
-                1,2
-            };
-
-            const unsigned int prefab_lightweight_spaceship_size{ 24 };
-            const unsigned int prefab_lightweight_spaceship_gridsize{ 5 };
-            const unsigned int prefab_lightweight_spaceship[24]
-            {
-                2,0, 3,0,
-                0,1, 1,1, 3,1, 4,1,
-                0,2, 1,2, 2,2, 3,2,
-                1,3, 2,3
-            };
-
-            const unsigned int prefab_middleweight_spaceship_size{ 22 };
-            const unsigned int prefab_middleweight_spaceship_gridsize{ 6 };
-            const unsigned int prefab_middleweight_spaceship[22]
-            {
-                0,0, 1,0, 2,0, 3,0, 4,0,
-                0,1, 5,1,
-                0,2,
-                1,3, 5,3,
-                3,4
-            };
-            
-            std::vector<Point_Cordinate> get_prefab
-                (unsigned int x,
-                 unsigned int y,
-                 unsigned int tile_size,
-                 const unsigned int prefab[],
-                 unsigned int size
-                 )
-            {
-                std::vector<Point_Cordinate> returnVector;
-                  
-                x = (x/tile_size) * tile_size;
-                y = (y/tile_size) * tile_size;  
-
-                for(int i = 0; i < size; i = i+2)
-                {
-                    returnVector.push_back(Point_Cordinate{ (prefab[i]*tile_size)+x, (prefab[i+1]*tile_size)+y });
-                }
-
-                return returnVector;
+                returnVector.push_back(Point_Cordinate{(prefab[i] * tile_size) + x, (prefab[i + 1] * tile_size) + y});
             }
 
-        public:
-            PreFabs_Factory(){}
+            return returnVector;
+        }
 
-            float selected_prefab_number = 0.0;
-            const unsigned int totalNumberOfPrefabs = 3;
-            enum prefab_names
+    public:
+        PreFabs_Factory() {}
+
+        float selected_prefab_number = 0.0;
+        const unsigned int totalNumberOfPrefabs = 4;
+        enum prefab_names
+        {
+            glider,
+            LightWeightSpaceship,
+            MiddleWeightSpaceShip,
+            HeavyWeightSpaceship
+        };
+
+        std::vector<Point_Cordinate> getPrefabByName(
+            unsigned int x, unsigned int y,
+            unsigned int tile_size,
+            prefab_names name, int number = -1)
+        {
+            int switch_number = 0;
+
+            if (number >= 0)
             {
-                glider,
-                LightWeightSpaceship,
-                MiddleWeightSpaceShip,
-                HeavyWeightSpaceship
-            };
-
-            std::vector<Point_Cordinate> getPrefabByName(
-                unsigned int x, unsigned int y,
-                unsigned int tile_size,
-                prefab_names name, int number = -1)
+                switch_number = number;
+            }
+            else
             {
-                int switch_number = 0;
-
-                if(number >= 0)
-                {
-                    switch_number = number;
-                }else
-                {
-                   switch_number = name;
-                }
-
-                switch(switch_number)
-                {
-                    case 0:
-                        return get_prefab(x, y, tile_size, prefab_glider, prefab_glider_size);
-                        break;
-                    case 1:
-                        return get_prefab(x, y, tile_size, prefab_lightweight_spaceship, prefab_lightweight_spaceship_size);
-                        break;
-                    case 2:
-                        return get_prefab(x, y, tile_size, prefab_middleweight_spaceship, prefab_middleweight_spaceship_size);
-                        break;
-                }
-
-                return std::vector<Point_Cordinate>{};
+                switch_number = name;
             }
 
-            //Select Prefab to insert by holding middle mouse button and scrolling to select.
-            void selectPrefab(sf::RenderWindow &window, sf::Event &event,  unsigned int tile_size)
+            switch (switch_number)
             {
-                if(sf::Mouse::isButtonPressed(sf::Mouse::Middle))
+            case 0:
+                return get_prefab(x, y, tile_size, prefab_glider, prefab_glider_size);
+                break;
+            case 1:
+                return get_prefab(x, y, tile_size, prefab_lightweight_spaceship, prefab_lightweight_spaceship_size);
+                break;
+            case 2:
+                return get_prefab(x, y, tile_size, prefab_middleweight_spaceship, prefab_middleweight_spaceship_size);
+                break;
+            case 3:
+                return get_prefab(x, y, tile_size, prefab_heavy_spaceship, prefab_heavy_spaceship_size);
+                break;
+            }
+
+            return std::vector<Point_Cordinate>{};
+        }
+
+        //Select Prefab to insert by holding middle mouse button and scrolling to select.
+        void selectPrefab(sf::RenderWindow &window, sf::Event &event, unsigned int tile_size)
+        {
+            if (sf::Mouse::isButtonPressed(sf::Mouse::Middle))
+            {
+                int x = (sf::Mouse::getPosition(window).x / tile_size) * tile_size;
+                int y = (sf::Mouse::getPosition(window).y / tile_size) * tile_size;
+
+                if (event.type == sf::Event::MouseWheelScrolled)
                 {
-                    int x = (sf::Mouse::getPosition(window).x/tile_size) * tile_size;
-                    int y = (sf::Mouse::getPosition(window).y/ tile_size) * tile_size;
-
-                    if(event.type == sf::Event::MouseWheelScrolled)
-                    {         
-                        if(event.mouseWheelScroll.delta > 0)
-                        {
-                            selected_prefab_number+=0.1;
-                            if(selected_prefab_number >= totalNumberOfPrefabs)
-                            {
-                                selected_prefab_number = totalNumberOfPrefabs - 1;
-                            }
-                            selectedVector.clear();
-                            selectedVector = getPrefabByName(x, y, tile_size, glider, selected_prefab_number);
-                        }else if(event.mouseWheelScroll.delta < 0)
-                        {
-                            selected_prefab_number-=0.1;
-                            if(selected_prefab_number < 0)
-                            {
-                                selected_prefab_number = 0;
-                            }
-                            selectedVector.clear();
-                            selectedVector = getPrefabByName(x, y, tile_size, glider, selected_prefab_number);
-                        }
-                    }
-
-                    for(Point_Cordinate point : selectedVector)
+                    if (event.mouseWheelScroll.delta > 0)
                     {
-                        sf::RectangleShape rect;
-                        rect.setSize(sf::Vector2f{ (float)tile_size, (float)tile_size });
-                        rect.setFillColor(sf::Color{ 252, 190, 3, 255 });
-                        rect.setPosition(point.x, point.y);
-                        window.draw(rect);
+                        selected_prefab_number += 0.1;
+                        if (selected_prefab_number >= totalNumberOfPrefabs)
+                        {
+                            selected_prefab_number = totalNumberOfPrefabs - 1;
+                        }
+                        selectedVector.clear();
+                        selectedVector = getPrefabByName(x, y, tile_size, glider, selected_prefab_number);
+                    }
+                    else if (event.mouseWheelScroll.delta < 0)
+                    {
+                        selected_prefab_number -= 0.1;
+                        if (selected_prefab_number < 0)
+                        {
+                            selected_prefab_number = 0;
+                        }
+                        selectedVector.clear();
+                        selectedVector = getPrefabByName(x, y, tile_size, glider, selected_prefab_number);
                     }
                 }
+
+                for (Point_Cordinate point : selectedVector)
+                {
+                    sf::RectangleShape rect;
+                    rect.setSize(sf::Vector2f{(float)tile_size, (float)tile_size});
+                    rect.setFillColor(sf::Color{252, 190, 3, 255});
+                    rect.setPosition(point.x, point.y);
+                    window.draw(rect);
+                }
             }
+        }
     };
-}
+} // namespace Prefabs
 
 #endif //PREFABS_CPP
